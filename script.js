14333ab0eb8f1f00eb4693164b290f64c94ab65c
Reugin <abhigaikwad735@gmail.com>
2025-07-10 20:24:18 +0530

    UI improved and added functionality

--- script.js
+++ script.js
@@ -7,30 +7,51 @@ var taskObject = [
     },
 ]
 
-
+//To display the add task window
 function addWindowToggle() {
     document.getElementById('addTaskWindow').style.display = "flex";
+    document.getElementById('main').style.filter = 'blur(3px)';
 }
 
+//To cancel add task window
 function cancel() {
     document.getElementById('addTaskWindow').style.display = "none";
+    document.getElementById('main').style.filter = 'blur(0px)';
 }
 
-
-
+//To add task in the list
 function addTaskButton() {
     let value = document.getElementById('newTaskInput').value;
 
-    var template = '<div class="listItems">' +
-        '<input type="checkbox"></input>' + value +
-        '</div>';
-
-    document.getElementById('taskList').innerHTML += template;
-
+    //if input in empty then alert to enter task
     if (!value) {
         alert('Please enter task');
     } else {
+        var template = '<div class="listItem">' +
+            '<input type="checkbox" onChange="isChecked()" id="checkbox"></input>' + '<span id="value">' + value + '<span/>' +
+            '</div>';
+        //to add task in list
+        document.getElementById('taskList').innerHTML += template;
+
+        //to hide add task window
         document.getElementById('addTaskWindow').style.display = "none";
         document.getElementById('newTaskInput').value = "";
+        document.getElementById('main').style.filter = 'blur(0px)';
     }
+}
+
+//To chcked checkbox and checnge the text decoration accordingly
+function isChecked() {
+    let toCheck = document.getElementById('checkbox').checked;
+    let taskCompleted = document.getElementById('value');
+
+    if (!toCheck) {
+        taskCompleted.style.textDecoration = "none";
+        taskCompleted.style.color = "#000";
+
+    } else { // if not checked then the text decoration will be line-through
+        taskCompleted.style.textDecoration = "line-through";
+        taskCompleted.style.color = "#aaa";
+    }
+
 }
